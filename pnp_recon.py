387bbe1eec1ed4307ef86f770bf38b4ebce23b68
Shulin-Hong <zqpshigui@sjtu.edu.cn>
2024-05-12 15:51:57 +0800

    简单实现pnp

--- pnp_recon.py
+++ pnp_recon.py
@@ -6,25 +6,29 @@ import open3d as o3d
 
 def solve_pnp_reconstruction(points3D, points2D):
     K = np.loadtxt('camera_intrinsic.txt')
-    # 使用PnP算法求解相机的位姿
-    # _, R_exp, t_exp = cv2.solvePnP(np.ascontiguousarray(points3D), points2D, K, np.zeros((5, 1)), flags=cv2.SOLVEPNP_ITERATIVE)
-
-    _, R_exp, t_exp = cv2.solvePnP(np.ascontiguousarray(points3D), points2D, K,None)
+    #flags=SOLVEPNP_EPNP
+    _, R_exp, t_exp = cv2.solvePnP(
+        np.ascontiguousarray(points3D),
+        points2D,
+        K,
+        np.zeros((5, 1)),
+        flags=cv2.SOLVEPNP_EPNP
+    )
 
     # 将旋转向量转换为旋转矩阵
     R, _ = cv2.Rodrigues(R_exp)
 
     # 创建4x4的齐次变换矩阵
-    camera_pose = np.hstack((R, t_exp))
-    camera_pose = np.vstack((camera_pose, np.array([0, 0, 0, 1])))
+    camera_pose = np.vstack((np.hstack((R, t_exp)), np.array([0, 0, 0, 1])))
 
-    points3D_for_projection = points3D.reshape(-1, 1, 3)
-    # 使用求解到的位姿反向投影2D点到3D空间
-    # 注意：rvec 和 tvec 应该是从 solvePnP 返回的旋转和平移向量
-    image_points, _ = cv2.projectPoints(points3D_for_projection, R_exp, t_exp, K, np.zeros((8, 1)))
+    # 为points3D添加一个1的列向量，使其成为齐次坐标 (n, 4)
+    points3D_homogeneous = np.hstack((points3D, np.ones((points3D.shape[0], 1))))
 
-    # 将返回的image_points转换回(n, 3)形状
-    points3d_recovered = image_points.reshape(-1, 3).T
+    # 使用相机位姿变换三维点到相机坐标系
+    points3D_transformed = camera_pose.dot(points3D_homogeneous.T).T
 
-    return camera_pose, points3d_recovered
+    # 截取前三个值作为变换后的三维点坐标
+    points3D_transformed = points3D_transformed[:, :3]
+
+    return camera_pose, points3D_transformed
 
